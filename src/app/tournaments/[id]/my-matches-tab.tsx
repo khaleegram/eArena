c3db2d7d0be3aba87351cb258d9e1abb493b8f8a
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-08-03 20:37:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/tournaments/[id]/my-matches-tab.tsx
+++ src/app/tournaments/[id]/my-matches-tab.tsx
@@ -25,7 +25,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../../components/ui/select";
 import { Separator } from "@/components/ui/separator";
 
@@ -1005,3 +1005,4 @@ export function ForceReplayAllDialog({ tournament, organizerId }: { tournament: 
 }
 
     
+
