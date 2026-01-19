ac92cefc9c3d81c832cccfd64edbf179844fffae
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2026-01-19 14:35:46 +0000

    i alreday have teh keys in enc also my being isntallable on android mean

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default async function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="eArena" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#A67C52" />
