eafd013abb1d812b1d1e513c47a2b52d3d23692b
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2026-01-19 22:42:50 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('@ducanh2912/next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   register: true,
   skipWaiting: true,
