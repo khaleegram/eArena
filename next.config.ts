856e0fc3bd58898749387924c3d87ac7127e32fd
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2026-01-26 19:20:29 +0000

    earlier push not is able to be being enebaled but now its not working it

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   sw: 'sw.js',
-  disable: process.env.NODE_ENV === 'development',
 });
 
 // Fix EventEmitter MaxListeners warning in development
